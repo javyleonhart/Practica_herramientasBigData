0e6aa4fce4973ee836a88bda4acaa5209b4bd83e
Javier Yañez <123128073+javyleonhart@users.noreply.github.com>
2024-04-13 15:41:54 -0300

    Add files via upload

---
+++ Paso00.sh
@@ -0,0 +1,14 @@
+sudo docker cp Datasets/canaldeventa/CanalDeVenta.csv namenode:/home/Datasets/CanalDeVenta.csv
+sudo docker cp Datasets/calendario/Calendario.csv namenode:/home/Datasets/Calendario.csv
+sudo docker cp Datasets/cliente/Cliente.csv namenode:/home/Datasets/Cliente.csv
+sudo docker cp Datasets/compra/Compra.csv namenode:/home/Datasets/Compra.csv
+sudo docker cp Datasets/empleado/Empleado.csv namenode:/home/Datasets/Empleado.csv
+sudo docker cp Datasets/gasto/Gasto.csv namenode:/home/Datasets/Gasto.csv
+sudo docker cp Datasets/producto/Producto.csv namenode:/home/Datasets/Producto.csv
+sudo docker cp Datasets/proveedor/Proveedor.csv namenode:/home/Datasets/Proveedor.csv
+sudo docker cp Datasets/sucursal/Sucursal.csv namenode:/home/Datasets/Sucursal.csv
+sudo docker cp Datasets/tipodegasto/TiposDeGasto.csv namenode:/home/Datasets/TiposDeGasto.csv
+sudo docker cp Datasets/venta/Venta.csv namenode:/home/Datasets/Venta.csv
+sudo docker cp Datasets/data_nvo/Cliente.csv namenode:/home/Datasets/Cliente_nvo.csv
+sudo docker cp Datasets/data_nvo/Empleado.csv namenode:/home/Datasets/Empleado_nvo.csv
+sudo docker cp Datasets/data_nvo/Producto.csv namenode:/home/Datasets/Producto_nvo.csv
